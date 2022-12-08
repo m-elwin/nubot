defe2aeba5fc324b0cb954321184ff11f007eb77
Matthew Elwin <elwin@northwestern.edu>
2022-12-08 10:55:20 -0600

    Added a simpler world

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         ('share/' + package_name + '/config', ['config/nubot_urdf.rviz']),
         ('share/' + package_name + '/urdf', ['urdf/nubot.urdf.xacro', 'urdf/nubot.gazebo.xacro']),
         ('share/' + package_name + '/env-hooks', ['env-hooks/nubot.dsv']),
-        ('share/' + package_name + '/worlds', ['worlds/nubot_world.sdf']),
+        ('share/' + package_name + '/worlds', ['worlds/nubot_world.sdf', 'worlds/nubot_simple.sdf']),
         ('share/' + package_name + '/launch', ['launch/simulate.launch.xml', 'launch/world.launch.xml', 'launch/nubot_rviz.launch.py'])
     ],
     install_requires=['setuptools'],
